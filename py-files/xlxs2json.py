7416aa326e799bc2315da35b0db95e15065e236d
thaoshibe <thao.nguyen@wisc.edu>
2025-02-15 00:13:57 -0600

    fix database

--- py-files/xlxs2json.py
+++ py-files/xlxs2json.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Load the Excel file
-df = pd.read_excel('abc.xlsx')
+df = pd.read_excel('../xlsx/words_with_examples.xlsx')
 
 # Convert DataFrame to JSON
 json_data = df.to_json(orient='records', force_ascii=False)
 
 # Save to a file
-with open('words_with_examples.json', 'w', encoding='utf-8') as f:
+with open('../words_with_examples.json', 'w', encoding='utf-8') as f:
     f.write(json_data)
 
 print('Conversion complete! JSON file saved as words_with_examples.json')
